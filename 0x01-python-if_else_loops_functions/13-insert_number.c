7c60d9a708b166ee37af93853c52ccba58178e1e
mihiALX <mohammedibrahimhamid.cs+alxgithub@gmail.com>
2023-10-31 21:22:08 +0300

    fix brinces

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -12,7 +12,7 @@ listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *current = *head, *new_node, *prev = NULL;
 
-	new_node = malloc(sizeof(listint_t);
+	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
 
