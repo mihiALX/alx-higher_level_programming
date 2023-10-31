11fa286854f96b4db8cea651de3b50b9902297ed
mihiALX <mohammedibrahimhamid.cs+alxgithub@gmail.com>
2023-10-31 20:59:12 +0300

    recode last digital file

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -9,8 +9,8 @@ if number < 0:
 else:
     last = number % 10
 if last > 5:
-    print("Last digit of {} is {}".format(number, last) + str1)
+    print(f"Last digit of {number} is {last}" + str1)
 elif last == 0:
-    print("Last digit of {} is {}".format(number, last) + str2)
+    print(f"Last digit of {number} is {last}" + str2)
 else:
-    print("Last digit of {} is {}".format(number, last) + str3)
+    print(f"Last digit of {number} is {last}" + str3)
