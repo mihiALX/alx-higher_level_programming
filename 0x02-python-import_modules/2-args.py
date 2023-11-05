fa02e2f0cf483a84b1bbed85c752b290a8317468
mihiALX <123060643+mihiALX@users.noreply.github.com>
2023-11-05 13:07:51 +0300

    Update 2-args.py4

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    n = len(sys.argv) - 1
-    if n == 0:
-        print('0 arguments.')
-    elif n == 1:
-        print('1 argument:')
+
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
     else:
-        print(f"{n} arguments:")
-    for i in range(n):
-        print('{}: {}'.format(i + 1, sys.argv[i + 1]))
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
