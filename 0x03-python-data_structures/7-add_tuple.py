5ca0ad7136c64cb7064f861057a5defe6f2e20a2
mihiALX <mohammedibrahimhamid.cs+alxgithub@gmail.com>
2023-11-06 23:29:25 +0300

    fix 7 add tuple file

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,6 +2,6 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     value1 = tuple_a[0] if len(tuple_a) > 0 else 0
     value2 = tuple_a[1] if len(tuple_a) > 1 else 0
-    value3 = tuple_b[0] if len(tuple_a) > 0 else 0
-    value4 = tuple_b[1] if len(tuple_a) > 1 else 0
+    value3 = tuple_b[0] if len(tuple_b) > 0 else 0
+    value4 = tuple_b[1] if len(tuple_b) > 1 else 0
     return(value1 + value3, value2 + value4)
