39a89c86ae84557cab30e4b42b24fa4528900a14
mihiALX <mohammedibrahimhamid.cs+alxgithub@gmail.com>
2023-11-08 20:20:15 +0300

    third push

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    return (max(a_dictionary) if a_dictionary else None)
+    if a_dictionary is None:
+        return None
+    maxvalue = 0
+    maxkey = None
+    for key, value in a_dictionary.items():
+        if value > maxvalue:
+            maxvalue = value
+            maxkey = key
+    return maxkey
