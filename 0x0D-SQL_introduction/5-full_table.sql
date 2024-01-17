8efe2b7fe1cc8d5bfeace69061dde7303c55cd66
mihialx <mohammedibrahimhamid.cs+alxgithub@gmail.com>
2024-01-17 22:09:35 +0300

    second push

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,2 +1,2 @@
--- a script that creates a table called first_table in the current database in your MySQL server.
-CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
+-- script that prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server 
+SHOW CREATE TABLE first_table
